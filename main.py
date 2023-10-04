699ce2b53a4cfd5e09fd8b9e79d52038b8facf36
Eva <ehd9094@yandex.ru>
2023-10-04 17:15:21 +0300

    fix: mistakes of flake8

--- main.py
+++ main.py
@@ -14,20 +14,17 @@ def degree(time: float, acceleration: float, radius: float, v_start: float = 0) 
 
     Returns:
         graduses between ball's start position, it is rounded for 2.
+
+    Raises:
+        Exception - radius equal or less zero.
     """
     if radius == 0:
         raise Exception("Ball's radius can't be a zero")
 
-    BALL_GRADUSES = 360
+    ball_graduses = 360
 
     ball_width = 2 * math.pi * radius
     distance = v_start * time + (acceleration * time**2) / 2
     between = distance % ball_width
 
-    return round(between / ball_width * BALL_GRADUSES, 2)
-
-
-print(degree(2, 2.5, 1))
-print(degree(5, 2.5, 1, 34))
-print(degree(18, 2.89, 123))
-print(degree(23, 2.35, 789))
+    return round(between / ball_width * ball_graduses, 2)
--- test_main.py
+++ test_main.py
@@ -2,22 +2,37 @@
 
 import pytest
 
-from main import degree
+import main
 
-
-data_test_degree = (
+data_test_degree_correct_cases = (
     ((2, 2.5, 1), 286.48),
     ((5, 2.5, 1, 34), 10.78),
     ((18, 2.89, 123), 218.09),
-    ((23, 2.35, 789), 45.14)
+    ((23, 2.35, 789), 45.14),
 )
 
 
-@pytest.mark.parametrize('args, expected', data_test_degree)
+@pytest.mark.parametrize('args, expected', data_test_degree_correct_cases)
 def test_degree_good(args, expected) -> None:
-    assert degree(*args) == expected
+    """Tests correct cases.
+
+    Args:
+        args (tuple[float, float, float]): it's a (time, acceleration, start speed).
+        expected (float): correct value.
+    """
+    assert main.degree(*args) == expected
+
+
+date_test_degree_error_cases = (
+    (0, 0, 0)
+)
 
 
 @pytest.mark.xfail(raises=Exception)
-def test_degree_error():
-    degree(0, 0, 0)
+def test_degree_error(args):
+    """Tests error cases.
+
+    Args:
+        args (tuple[float, float, float]): it's a (time, acceleration, start speed).
+    """
+    main.degree(*args)
